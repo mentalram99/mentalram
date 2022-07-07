61991b7ed852d686947ae489c544c6d40539bdfc
mentalram99 <mentalramku@gmail.com>
2022-07-07 11:59:34 +0530

    buddy

---
+++ Log4j1.java
@@ -0,0 +1,16 @@
+package UsingLog4j;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class Log4j1 {
+  static Logger log=Logger.getLogger(Log4j1.class);
+  public static void main(String[] args) {
+	  BasicConfigurator.configure();
+	  log.info("Ok");
+	  log.warn("No");
+	  log.error("Use");
+	  log.fatal("Fat");
+	  log.debug("Got");
+  }
+}
